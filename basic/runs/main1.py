f76d8fbfd7554221a77711d09f4cd153cc94d601
Klaus Havelund <klaus.havelund@jpl.nasa.gov>
2024-09-26 13:54:02 -0700

    experimenting with command constraints

--- basic/runs/main1.py
+++ basic/runs/main1.py
@@ -12,7 +12,20 @@ ARGS = 'args'
 ZONE = 'zone'
 MODE = 'mode'
 
-def cc_C7(cmd: Command) -> bool:
+""" Parameter types for zone and mode:
+    'C1': AB
+    'C2': BC
+    'C3': CD
+    'C4': DE
+    'C5': EA
+    'C6': AB
+    'C7': BC
+    'C8': CD
+    'C9': DE
+    'C10': EA
+"""
+
+def C7_b1_or_b2_and_not_c3(cmd: Command) -> bool:
     name = cmd[NAME]
     args = cmd[ARGS]
     return name == 'C7' and args[ZONE] in {'b1', 'b2'} and args[MODE] != 'c3'
@@ -27,23 +40,10 @@ constraints1: list[TestConstraint] = [
 
 constraints2: list[TestConstraint] = [
     Eventually(N('C1')),
-    Always(Implies(Now(Cmd('C1')), Eventually(Now(cc_C7))))
+    Always(Implies(N('C1'), Eventually(Now(C7_b1_or_b2_and_not_c3))))
 ]
 
 
 if __name__ == '__main__':
     tests = generate_tests(cmdDict, enumDict, constraints2, 10, 5)
     pp(tests)
-
-"""
-    'C1': AB
-    'C2': BC
-    'C3': CD
-    'C4': DE
-    'C5': EA
-    'C6': AB
-    'C7': BC
-    'C8': CD
-    'C9': DE
-    'C10': EA
-"""
