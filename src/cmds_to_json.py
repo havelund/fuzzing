31b0ac35cc7c22f57cba0f7afb06dfec6df53835
Klaus Havelund <klaus.havelund@jpl.nasa.gov>
2024-10-12 11:12:08 -0700

    finished script that converts dicts from .py modules to json

--- src/cmds_to_json.py
+++ src/cmds_to_json.py
@@ -1,40 +1,80 @@
 
 import argparse
 import json
-import importlib
+import importlib.util
+import os
+
+def import_all_from_directory(directory: str):
+    """ Imports and returns all Python modules in a given directory.
+
+    The Python modules are assumed to each contain a `enumDict` and a `cmdDict`,
+    containing respectively the types of command arguments, and the commands
+    themselves.
+
+    :param directory: path to directory containing Python modules (`.py` files).
+    :return: list of imported Python modules.
+    """
+    imported_modules = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".py") and filename != "__init__.py":
+            file_path = os.path.join(directory, filename)
+            module_name = os.path.basename(file_path).replace('.py', '')
+            spec = importlib.util.spec_from_file_location(module_name, file_path)
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            imported_modules.append(module)
+    return imported_modules
 
 
-def write_json_file(py_module: str, json_file: str):
-    data = importlib.import_module(py_module.replace('.py', ''))
+def write_json_file(py_dir: str, json_file: str):
+    """Reads Python modules with enum and command dictionaries and writes a json file
+    containing all these.
+
+    :param py_dir: path to directory containing Python modules (`.py` files).
+    :param json_file: Json file to which all enum and command specifications are written.
+    """
+
+    modules = import_all_from_directory(py_dir)
+    enum_dict: dict = {}
+    cmd_dict: dict = {}
+    for module in modules:
+        print(f"Module name: {module.__name__}")
+        if hasattr(module, 'enumDict'):
+            print(f"  updating enumDict")
+            enum_dict.update(module.enumDict)
+        if hasattr(module, 'cmdDict'):
+            print(f"  updating cmdDict")
+            cmd_dict.update(module.cmdDict)
     dictionary = {
-        "enum_dict": data.enumDict,
-        "cmd_dict": data.cmdDict
+        "enum_dict": enum_dict,
+        "cmd_dict": cmd_dict
     }
     with open(json_file, "w") as json_file:
         json.dump(dictionary, json_file, indent=4)
 
 
 def main(args = None):
-    # Obtain names of input file and output file:
+    # Define input arguments to script:
     parser = argparse.ArgumentParser(description="Dictionary json file generator")
-    parser.add_argument("py_module", help="command and enum Python module")
-    parser.add_argument("json_file", help="command and enum Json file")
+    parser.add_argument("py_dir", help="path to directory containing Python modules with dictionaries")
+    parser.add_argument("json_file", help="resulting command and enum Json file")
+    # Parse provided input arguments:
     parsed_args = parser.parse_args(args)
-    py_module = parsed_args.py_module
+    py_dir = parsed_args.py_dir
     json_file = parsed_args.json_file
-    print('Reading from:')
-    print(f'  {py_module}')
+    print('Reading from directry:')
+    print(f'  {py_dir}')
     print('Writing to:')
     print(f'  {json_file}')
-
-    # Generate file
-    write_json_file(py_module, json_file)
+    # Translate Python modules in `py_dir` to Json, written to `json_file`:
+    write_json_file(py_dir, json_file)
 
 
 if __name__ == '__main__':
+    # Testing script:
+    datadir = '/Users/khavelun/Desktop/development/pycharmworkspace/fuzzing/data/'
     args = [
-        'data.input.dict_clipper1.py',
-        '../data/output/cmd_enum.json',
+        datadir + 'input/clipper1',
+        datadir + 'output/cmd_enum.json'
     ]
     main(args)
-
