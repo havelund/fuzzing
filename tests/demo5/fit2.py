2583ba15fd758c3bb6e096a277ed4c1f711cb563
Klaus Havelund <klaus.havelund@jpl.nasa.gov>
2025-03-19 16:05:43 -0700

    Remove demo from version control

--- tests/demo5/fit2.py
+++
@@ -1,9 +0,0 @@
-
-import json
-
-if __name__ == '__main__':
-    with open('fuzz-testsuite.json', 'r') as file: tests = json.load(file)
-    for test in tests:
-        print(f'RESET_FSW')
-        for cmd in test:
-            print(cmd)
